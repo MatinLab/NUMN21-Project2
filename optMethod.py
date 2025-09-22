655e3caaab4cb030da46f281487f2c680393fee8
CHMN-LU <co2441ha-s@student.lu.se>
2025-09-22 14:22:20 +0200

    Setup of optProblem and optMethod classes

--- optMethod.py
+++ optMethod.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+class OptMethod():
+    # Generic class for Quasi Newton methods
+    # Constructor
+    def __init__(self):
+        pass
+    # Optimize method
+    def optimize(self, function, gradient=None):
+        # To be overridden in the child class
+        pass
+    
+    
+class Newton(OptMethod):
+    # Does Newton method
+    def optimize(self, function, gradient=None):
+        # Run the optimization on the given function with optionally the given gradient
+        pass
