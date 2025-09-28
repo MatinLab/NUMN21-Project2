Implemented exact line search using binary search for minimizing alpha. Fixed some numerical errors arising due to step sizes. Modified git ignore to ignore .pyc files

optMethod.py
@@ -10,20 +10,26 @@ class OptMethod():
         # To be overridden in the child class
         pass
     # Numerically approximate the gradient of a function
-    def approximate_grad(self, f, x, dx=10**-7):
+    def approximate_grad(self, f, x, dx=10**-4):
         # Approximate the gradient of a function f about the vector x
         # x must be a numpy array and f must take numpy arrays as arguments
         # dx is the delta to add to each variable
-        grad = np.zeros(x.shape)
+        grad = np.zeros(x.shape, dtype=np.float64)
         f0 = f(x)
         for i in range(len(grad)):
             x1 = x.copy()
             x1[i] += dx
             grad[i] = (f(x1)-f0)/dx
         return grad
+    # Numerically approximate the gradient of a 1D function
+    def approximate_grad_1D(self, f, x, dx=10**-4):
+        # Approximate the gradient of a function f at x
+        # x must be a float and f must take a single float as argument
+        # dx is the delta to add to x
+        return (f(x+dx)-f(x))/dx
     # Function to approximate the Hessian of a function
-    def approximate_hessian(self, f, x, dx=10**-7):
-        H = np.zeros((len(x), len(x)))
+    def approximate_hessian(self, f, x, dx=10**-4):
+        H = np.zeros((len(x), len(x)), dtype=np.float64)
         grad0 = self.approximate_grad(f, x, dx)
         for i in range(len(x)):
             # For each xi, we get the derivative of the gradient with
@@ -33,6 +39,7 @@ class OptMethod():
             x1[i] += dx
             H[i] = (self.approximate_grad(f, x1, dx)-grad0)/dx
         return H
+    
     # STOPPING CRITERIA
     # A stopping criterion function should take the current point,
     # the next point, the gradient, the inverse Hessian, and the tolerance.
@@ -52,10 +59,69 @@ class OptMethod():
         if np.linalg.norm(x1-x0) < tol:
             return True
         return False
+    # Binomial search function
+    def binary_minimum(self, f, a, b, tol=10**-4, max_iterations=10**5):
+        # Search for a minimum of f on the interval [a, b]
+        # Perform binary search on the gradient of f
+        # 1d function f
+        ga = self.approximate_grad_1D(f, a)
+        gb = self.approximate_grad_1D(f, b)
+        assert not ga*gb>0, "Derivative must change sign in the interval"
+        i = 0
+        while b-a > tol:
+            # If our interval isn't small enough, keep looking
+            m = (b+a)/2
+            gm = self.approximate_grad_1D(f, m)
+            if gm*ga < 0: # sign change between a and m
+                # Minimum is between a and m
+                b = m
+                gb = gm
+            elif gm*gb < 0: # sign change between m and b
+                # Min between m and b
+                a = m
+                ga = gm
+            else:
+                # m is the minimum
+                return m
+            i += 1
+            if i > max_iterations:
+                print("Minimum not found to specified tolerance in the maximum number of iterations")
+                print(f"At end interval [a, b]=[{a}, {b}]")
+                break
+        return (b+a)/2
+                
+        
         
 class Newton(OptMethod):
     # Newtons method for finding the root of the gradient
-    def optimize(self, function, x0, gradient=None, dx=10**-7, termination_criterion=None, tol=10**-4, max_iterations=10**5):
+    def optimize(self, function, x0, gradient=None, dx=10**-4, termination_criterion=None, tol=10**-4, alpha=None, max_iterations=10**5):
+        """
+        Parameters
+        ----------
+        function :
+            Use Newton's method to calculate the minimum of a given function.
+        x0 : ndarray
+            Starting point.
+        gradient : function, optional
+            A function which takes a function, a point, and a delta dx. Should return the gradient of the function at the point. The default is None.
+        dx : float, optional
+            Delta dx for numerically approximating the gradient and/or Hessian. The default is 10**-4.
+        termination_criterion : function, optional
+            A function which takes the starting point, next point in the method, gradient, inverse Hessian, and tolerance.
+            Returns True when termination should occur. The default is None.
+        tol : float, optional
+            Tolerance within which termination should occur in termination_criterion. The default is 10**-4.
+        alpha : float, optional
+            Step size in Newton's method. If not specified, exact line search is used. The default is 1.
+        max_iterations : int, optional
+            Maximum iterations to perform before terminating. The default is 10**5.
+
+        Returns
+        -------
+        x1 : ndarray
+            The point at which a minimum is found.
+
+        """
         # Run the optimization on the given function with optionally the given gradient
         # Returns the point at which the gradient was found to be zero
         # Default to residual if no criterion provided
@@ -68,24 +134,57 @@ class Newton(OptMethod):
         H_inv = np.linalg.inv(self.approximate_hessian(function, x0, dx))
         grad = gradient(function, x0, dx)
         Sk = -1*H_inv @ grad
-        x1 = x0 + Sk
+        if alpha is None:
+            alpha = self.exact_line_search(function, x0, grad, H_inv, dx, tol)
+        x1 = x0 + alpha*Sk
         i = 0
         while not termination_criterion(x0, x1, grad, H_inv, tol):
             # Hasn't converged, make another step
             x0 = x1.copy()
-            #try:
-                #H_inv = np.linalg.inv(self.approximate_hessian(function, x0, dx))
-            #except np.linalg.LinAlgError:
-                #print("LINALGERROR")
-                #rint(x0)
-                #print(self.approximate_hessian(function, x0, dx))
+            try:
+                H = self.approximate_hessian(function, x0, dx)
+                H_inv = np.linalg.inv(H)
+            except np.linalg.LinAlgError:
+                print(f"LINALGERROR, iteration {i}")
+                print(x0)
+                print(H)
+                exit()
             H_inv = np.linalg.inv(self.approximate_hessian(function, x0, dx))
             grad = gradient(function, x0, dx)
             Sk = -1*H_inv @ grad
-            x1 = x0 + Sk
+            if alpha is None:
+                alpha = self.exact_line_search(function, x0, grad, H_inv, dx, tol)
+            x1 = x0 + alpha*Sk
             # Backup termination
             i += 1
             if i > max_iterations:
                 print("Maximum iterations achieved without convergence")
                 break
         return x1
+    
+    # Exact line search method
+    def exact_line_search(self, f, x, grad, H_inv, dx=10**-4, tol=10**-4):
+        # Find the minimum of the function f(alpha) = f(x+alpha*(-H_inv@grad))
+        # Using binary search
+        # Find an interval [0, b] on which the sign of the derivative of f(alpha) changes
+        Sk = -1*H_inv @ grad
+        f_alpha = lambda a: f(x+a*Sk)
+        a = 0
+        b = 0.1
+        ga = self.approximate_grad_1D(f_alpha, a, dx)
+        gb = self.approximate_grad_1D(f_alpha, b, dx)
+        while gb*ga > 0: # while no sign change
+            a = b
+            b += 0.1
+            gb = self.approximate_grad_1D(f_alpha, b, dx)
+            # Terminate if b has gotten too high
+            if b > 5:
+                # b too high, use alpha of 1
+                #print("Bounds for alpha exceeded, using alpha=1")
+                return 1
+        # Sign change found at b
+        #print(f"sign change found on interval [{a}, {b}]")
+        # Find the minimum on the interval a, b
+        return self.binary_minimum(f_alpha, a, b, tol)
+        
+    

testing_methods.py
@@ -7,20 +7,27 @@ testFunction2 = lambda x: x[0]**2 + x[1]**2 - 1  # Should have minimum at (0, 0)
 
 newton_method = Newton()
 
-x0 = np.array([1, 1.])
-#H = newton_method.approximate_hessian(testFunction1, x0)
+x0 = np.array([1, -1.])
+H = newton_method.approximate_hessian(testFunction1, x0)
 #print(H)
-#print(np.linalg.inv(H))
+H_inv = np.linalg.inv(H)
+
+# Testing exact line search
+grad = newton_method.approximate_grad(testFunction1, x0)
+alpha = newton_method.exact_line_search(testFunction1, x0, grad, H_inv)
+print(f"Minimizing alpha: {alpha}")
+
+dx = 10**-4
 print("Minimum for x**3 + 2xy + y**6")
-minimum = newton_method.optimize(testFunction1, x0, dx=10**-7, termination_criterion=newton_method.residual_criterion, tol=10**-5)
+minimum = newton_method.optimize(testFunction1, x0, dx=dx, termination_criterion=newton_method.residual_criterion, tol=10**-3)
 print(f"Minimum of function found at {minimum} using residual stopping criterion")
-minimum = newton_method.optimize(testFunction1, x0, dx=10**-7, termination_criterion=newton_method.cauchy_criterion, tol=10**-5)
+minimum = newton_method.optimize(testFunction1, x0, dx=dx, termination_criterion=newton_method.cauchy_criterion, tol=10**-3)
 print(f"Minimum of function found at {minimum} using Cauchy stopping criterion")
 
 print("Minimum for x**2 + y**2 - 1")
-minimum = newton_method.optimize(testFunction2, x0, dx=10**-7, termination_criterion=newton_method.residual_criterion, tol=10**-5)
+minimum = newton_method.optimize(testFunction2, x0, dx=dx, termination_criterion=newton_method.residual_criterion, tol=10**-3)
 print(f"Minimum of function found at {minimum} using residual stopping criterion")
-minimum = newton_method.optimize(testFunction2, x0, dx=10**-7, termination_criterion=newton_method.cauchy_criterion, tol=10**-5)
+minimum = newton_method.optimize(testFunction2, x0, dx=dx, termination_criterion=newton_method.cauchy_criterion, tol=10**-3)
 print(f"Minimum of function found at {minimum} using Cauchy stopping criterion")
 
 