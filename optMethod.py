d163939de1015759e471325dd6c6d967ccf040e9
CHMN-LU <co2441ha-s@student.lu.se>
2025-09-29 23:18:16 +0200

    Task 9, implemented and tested all Quasi Newton methods.

--- optMethod.py
+++ optMethod.py
@@ -4,7 +4,8 @@ class OptMethod():
     # Generic class for Quasi Newton methods
     # Constructor
     def __init__(self):
-        pass
+        self.H = None # Store current Hessian approximation
+        self.H_inv = None # Store current Hessian inverse approximation
     # Optimize method
     def optimize(self, function, gradient=None):
         # To be overridden in the child class
@@ -14,7 +15,7 @@ class OptMethod():
         # Approximate the gradient of a function f about the vector x
         # x must be a numpy array and f must take numpy arrays as arguments
         # dx is the delta to add to each variable
-        grad = np.zeros(x.shape, dtype=np.float64)
+        grad = np.zeros(x.shape, dtype=np.double)
         f0 = f(x)
         for i in range(len(grad)):
             x1 = x.copy()
@@ -27,18 +28,46 @@ class OptMethod():
         # x must be a float and f must take a single float as argument
         # dx is the delta to add to x
         return (f(x+dx)-f(x))/dx
-    # Function to approximate the Hessian of a function
-    def approximate_hessian(self, f, x, dx=10**-4):
-        H = np.zeros((len(x), len(x)), dtype=np.float64)
-        grad0 = self.approximate_grad(f, x, dx)
+    # Function to instantiate the inverse of the Hessian using a numerical approximation
+    # This can be overriden as desired separately from approximate_hessian below
+    # though they start the same
+    def instantiate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        # Accept optional parameter x0 for compatability with Quasi Newton approximations
+        if gradient is None:
+            gradient = self.approximate_grad
+        H = np.zeros((len(x), len(x)), dtype=np.double)
+        grad0 = gradient(f, x, dx)
         for i in range(len(x)):
             # For each xi, we get the derivative of the gradient with
             # respect to the xi
             # This is the ith row in the Hessian
             x1 = x.copy()
             x1[i] += dx
-            H[i] = (self.approximate_grad(f, x1, dx)-grad0)/dx
-        return H
+            H[i] = (gradient(f, x1, dx)-grad0)/dx
+        H_inv = np.linalg.inv(H)
+        # Store Hessians
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
+    # Function to numerically approximate the inverse of the Hessian of a function
+    def approximate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        # Accept optional parameter x0 for compatability with Quasi Newton approximations
+        if gradient is None:
+            gradient = self.approximate_grad
+        H = np.zeros((len(x), len(x)), dtype=np.double)
+        grad0 = gradient(f, x, dx)
+        for i in range(len(x)):
+            # For each xi, we get the derivative of the gradient with
+            # respect to the xi
+            # This is the ith row in the Hessian
+            x1 = x.copy()
+            x1[i] += dx
+            H[i] = (gradient(f, x1, dx)-grad0)/dx
+        H_inv = np.linalg.inv(H)
+        # Store Hessians
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
     
     # STOPPING CRITERIA
     # A stopping criterion function should take the current point,
@@ -96,9 +125,34 @@ class OptMethod():
         # Then specify that you want to use it as a parameter in optimize
         # Don't need self if the function is defined outside a class
         pass
+    
+    # Exact line search method
+    def exact_line_search(self, f, x, grad, H_inv, dx=10**-4, tol=10**-4):
+        # Find the minimum of the function f(alpha) = f(x+alpha*(-H_inv@grad))
+        # Using binary search
+        # Find an interval [0, b] on which the sign of the derivative of f(alpha) changes
+        Sk = -1*H_inv @ grad
+        f_alpha = lambda a: f(x+a*Sk)
+        a = 0
+        b = 0.1
+        ga = self.approximate_grad_1D(f_alpha, a, dx)
+        gb = self.approximate_grad_1D(f_alpha, b, dx)
+        while gb*ga > 0: # while no sign change
+            a = b
+            b += 0.1
+            gb = self.approximate_grad_1D(f_alpha, b, dx)
+            # Terminate if b has gotten too high
+            if b > 3:
+                # b too high, use alpha of 1
+                #print("Bounds for alpha exceeded, using alpha=1")
+                return 1
+        # Sign change found at b
+        #print(f"sign change found on interval [{a}, {b}]")
+        # Find the minimum on the interval a, b
+        return self.binary_minimum(f_alpha, a, b, tol)
                 
         
-        
+# Newton's method      
 class Newton(OptMethod):
     # Newtons method for finding the root of the gradient
     def optimize(self, function, x0, gradient=None, dx=10**-4, termination_criterion=None, tol=10**-4, alpha=None, line_search=None, max_iterations=10**5):
@@ -145,61 +199,170 @@ class Newton(OptMethod):
         if line_search is None:
             line_search = self.exact_line_search
         # Run Newton's method
-        H_inv = np.linalg.inv(self.approximate_hessian(function, x0, dx))
-        grad = gradient(function, x0, dx)
-        Sk = -1*H_inv @ grad
-        if alpha is None:
-            alpha = line_search(function, x0, grad, H_inv, dx, tol)
-        x1 = x0 + alpha*Sk
+        H_inv = self.instantiate_hessian_inv(function, x0, gradient, dx)
+        #H_inv = np.linalg.inv(self.H)
+        #grad = gradient(function, x0, dx)
+        #Sk = -1*H_inv @ grad
+        #if alpha is None:
+            #alpha = line_search(function, x0, grad, H_inv, dx, tol)
+        #x1 = x0 + alpha*Sk
         i = 0
-        while not termination_criterion(x0, x1, grad, H_inv, tol):
+        while True:
             # Hasn't converged, make another step
-            x0 = x1.copy()
-            # REMOVE TRY CATCH LATER
-            try:
-                H = self.approximate_hessian(function, x0, dx)
-                H_inv = np.linalg.inv(H)
-            except np.linalg.LinAlgError:
-                print(f"LINALGERROR, iteration {i}")
-                print(x0)
-                print(H)
-                exit()
-            H_inv = np.linalg.inv(self.approximate_hessian(function, x0, dx))
             grad = gradient(function, x0, dx)
             Sk = -1*H_inv @ grad
             if alpha is None:
                 alpha = line_search(function, x0, grad, H_inv, dx, tol)
             x1 = x0 + alpha*Sk
+            # Check termination condition
+            if termination_criterion(x0, x1, grad, H_inv, tol):
+                break
+            # else we continue
+            # Update Hessian for the next step
+            H_inv = self.approximate_hessian_inv(function, x1, gradient, dx, x0)
+            # Set new x0
+            x0 = x1.copy()
             # Backup termination
             i += 1
             if i > max_iterations:
                 print("Maximum iterations achieved without convergence")
                 break
         return x1
-    
-    # Exact line search method
-    def exact_line_search(self, f, x, grad, H_inv, dx=10**-4, tol=10**-4):
-        # Find the minimum of the function f(alpha) = f(x+alpha*(-H_inv@grad))
-        # Using binary search
-        # Find an interval [0, b] on which the sign of the derivative of f(alpha) changes
-        Sk = -1*H_inv @ grad
-        f_alpha = lambda a: f(x+a*Sk)
-        a = 0
-        b = 0.1
-        ga = self.approximate_grad_1D(f_alpha, a, dx)
-        gb = self.approximate_grad_1D(f_alpha, b, dx)
-        while gb*ga > 0: # while no sign change
-            a = b
-            b += 0.1
-            gb = self.approximate_grad_1D(f_alpha, b, dx)
-            # Terminate if b has gotten too high
-            if b > 5:
-                # b too high, use alpha of 1
-                #print("Bounds for alpha exceeded, using alpha=1")
-                return 1
-        # Sign change found at b
-        #print(f"sign change found on interval [{a}, {b}]")
-        # Find the minimum on the interval a, b
-        return self.binary_minimum(f_alpha, a, b, tol)
         
-    
+# Quasi Newton methods
+
+# Good Broyden method
+class GoodBroyden(Newton):
+    # Perform Newton's method style steps, but approximate the Hessian using a "good" Broyden rank 1 update and
+    # the Sherman-Morisson computation of the new matrix
+    # Broyden update to the Hessian
+    def broyden_update(self, function, x0, x1, gradient, dx):
+        # Define delta and lambda values as
+        # deltak = xk+1-xk and lambdak=gradk+1-gradk
+        delta_k = x1-x0
+        lambda_k = gradient(function, x1, dx)-gradient(function, x0, dx)
+        # Reshaping vectors
+        delta_k = delta_k.reshape((-1, 1))
+        lambda_k = lambda_k.reshape((-1, 1))
+        H_inv = self.H_inv
+        # Then use Sherman-Morisson formula
+        summed_value = ((delta_k-(H_inv @ lambda_k))/(delta_k.T @ H_inv @ lambda_k)) @ (delta_k.T @ H_inv)
+        Hkplus1_inv = H_inv + summed_value
+        Hkplus1 = np.linalg.inv(Hkplus1_inv)
+        return Hkplus1, Hkplus1_inv
+    # Override the approximate_hessian_inv function
+    def approximate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        assert x0 is not None, "Previous point in the method required for Broyden step"
+        H, H_inv = self.broyden_update(f, x0, x, gradient, dx)
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
+
+# Bad Broyden method
+class BadBroyden(Newton):
+    # Perform Newton's method style steps, but approximate the Hessian using a "bad" Broyden rank 1 update
+    # Broyden update to the Hessian
+    def broyden_update(self, function, x0, x1, gradient, dx):
+        # Define delta and lambda values as
+        # deltak = xk+1-xk and lambdak=gradk+1-gradk
+        delta_k = x1-x0
+        lambda_k = gradient(function, x1, dx)-gradient(function, x0, dx)
+        # Reshaping vectors
+        delta_k = delta_k.reshape((-1, 1))
+        lambda_k = lambda_k.reshape((-1, 1))
+        H_inv = self.H_inv
+        # Then use bad broyden update
+        Hkplus1_inv = H_inv + ((delta_k - (H_inv @ lambda_k))/(lambda_k.T @ lambda_k)) @ lambda_k.T
+        Hkplus1 = np.linalg.inv(Hkplus1_inv)
+        return Hkplus1, Hkplus1_inv
+    # Override the approximate_hessian_inv function
+    def approximate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        assert x0 is not None, "Previous point in the method required for Broyden step"
+        H, H_inv = self.broyden_update(f, x0, x, gradient, dx)
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
+
+
+# Symmetric Broyden method
+class SymmetricBroyden(Newton):
+    # Perform Newton's method style steps, but approximate the Hessian using a symmetric Broyden rank 1 update
+    # Broyden update to the Hessian
+    def broyden_update(self, function, x0, x1, gradient, dx):
+        # Define delta and lambda values as
+        # deltak = xk+1-xk and lambdak=gradk+1-gradk
+        delta_k = x1-x0
+        lambda_k = gradient(function, x1, dx)-gradient(function, x0, dx)
+        # Reshaping vectors
+        delta_k = delta_k.reshape((-1, 1))
+        lambda_k = lambda_k.reshape((-1, 1))
+        H_inv = self.H_inv
+        # Define u and a
+        u_k = delta_k - (H_inv @ lambda_k)
+        a_k = 1 / (u_k.T @ lambda_k)
+        # Make a symmetric update
+        Hkplus1_inv = H_inv + (a_k * (u_k @ u_k.T))
+        Hkplus1 = np.linalg.inv(Hkplus1_inv)
+        return Hkplus1, Hkplus1_inv
+    # Override the approximate_hessian_inv function
+    def approximate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        assert x0 is not None, "Previous point in the method required for Broyden step"
+        H, H_inv = self.broyden_update(f, x0, x, gradient, dx)
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
+
+# DFP method
+class DFP(Newton):
+    # Perform Newton's method style steps, but approximate the Hessian using a DFP rank 2 update
+    # DFP update to the Hessian
+    def dfp_update(self, function, x0, x1, gradient, dx):
+        # Define delta and lambda values as
+        # deltak = xk+1-xk and lambdak=gradk+1-gradk
+        delta_k = x1-x0
+        lambda_k = gradient(function, x1, dx)-gradient(function, x0, dx)
+        # Reshaping vectors
+        delta_k = delta_k.reshape((-1, 1))
+        lambda_k = lambda_k.reshape((-1, 1))
+        H_inv = self.H_inv
+        # Make a rank 2 update
+        term1 = (delta_k @ delta_k.T)/(delta_k.T @ lambda_k)
+        term2 = (H_inv @ lambda_k @ lambda_k.T @ H_inv)/(lambda_k.T @ H_inv @ lambda_k)
+        Hkplus1_inv = H_inv + term1 - term2
+        Hkplus1 = np.linalg.inv(Hkplus1_inv)
+        return Hkplus1, Hkplus1_inv
+    # Override the approximate_hessian_inv function
+    def approximate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        assert x0 is not None, "Previous point in the method required for DFP step"
+        H, H_inv = self.dfp_update(f, x0, x, gradient, dx)
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
+
+# BFGS method
+class BFGS(Newton):
+    # Perform Newton's method style steps, but approximate the Hessian using a BFGS rank 2 update
+    # BFGS update to the Hessian
+    def bfgs_update(self, function, x0, x1, gradient, dx):
+        # Define delta and lambda values as
+        # deltak = xk+1-xk and lambdak=gradk+1-gradk
+        delta_k = x1-x0
+        lambda_k = gradient(function, x1, dx)-gradient(function, x0, dx)
+        # Reshaping vectors
+        delta_k = delta_k.reshape((-1, 1))
+        lambda_k = lambda_k.reshape((-1, 1))
+        H_inv = self.H_inv
+        # Make a rank 2 update
+        term1_factor = 1 + (lambda_k.T @ H_inv @ lambda_k)/(delta_k.T @ lambda_k)
+        term1 = (delta_k @ delta_k.T)/(delta_k.T @ lambda_k)
+        term2 = ((delta_k @ lambda_k.T @ H_inv) + (H_inv @ lambda_k @ delta_k.T))/(delta_k.T @ lambda_k)
+        Hkplus1_inv = H_inv + (term1_factor * term1) - term2
+        Hkplus1 = np.linalg.inv(Hkplus1_inv)
+        return Hkplus1, Hkplus1_inv
+    # Override the approximate_hessian_inv function
+    def approximate_hessian_inv(self, f, x, gradient=None, dx=10**-4, x0=None):
+        assert x0 is not None, "Previous point in the method required for BFGS step"
+        H, H_inv = self.bfgs_update(f, x0, x, gradient, dx)
+        self.H = H
+        self.H_inv = H_inv
+        return H_inv
